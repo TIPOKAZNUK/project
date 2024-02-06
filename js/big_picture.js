79e5dd7bf9a96eb6b93a7dd81907ac58b5bdbd90
TIPOKAZNUK <tipokaznuk@gmail.com>
2024-02-06 19:25:03 +0300

    comment template

--- js/big_picture.js
+++ js/big_picture.js
@@ -9,6 +9,8 @@ const socialCaption = bigPicture.querySelector('.social__caption');
 const body = document.querySelector('body');
 const closeBtn = bigPicture.querySelector('.big-picture__cancel');
 
+const commentTemplate = document.querySelector('#comment-template').content.querySelector('.social__comment');
+
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
@@ -27,6 +29,21 @@ const onCloseBtnClick = () => {
   closeBigPicture();
 };
 
+const createCommentElement = (comment) => {
+  const commentElement = commentTemplate.cloneNode(true);
+
+  const commentImg = commentElement.querySelector('.social__picture');
+  commentImg.src = comment.avatar;
+  commentImg.alt = comment.name;
+  commentImg.width = 35;
+  commentImg.height = 35;
+
+  const commentText = commentElement.querySelector('.social__text');
+  commentText.textContent = comment.message;
+
+  return commentElement;
+};
+
 const openBigPicture = (photo) => {
   bigPictureImg.src = photo.url;
   likesCount.textContent = photo.likes;
@@ -36,23 +53,7 @@ const openBigPicture = (photo) => {
   socialComments.innerHTML = ''; // or use socialComments.innerHTML = '';
 
   photo.comments.forEach((comment) => {
-    const commentElement = document.createElement('li');
-    commentElement.classList.add('social__comment');
-
-    const commentImg = document.createElement('img');
-    commentImg.classList.add('social__picture');
-    commentImg.src = comment.avatar;
-    commentImg.alt = comment.name;
-    commentImg.width = 35;
-    commentImg.height = 35;
-
-    const commentText = document.createElement('p');
-    commentText.classList.add('social__text');
-    commentText.textContent = comment.message;
-
-    commentElement.appendChild(commentImg);
-    commentElement.appendChild(commentText);
-
+    const commentElement = createCommentElement(comment);
     socialComments.appendChild(commentElement);
   });
 
