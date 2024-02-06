7ce7b8ea6fa9e1eb22a61481304ba6def7cefec1
TIPOKAZNUK <tipokaznuk@gmail.com>
2024-02-06 16:17:09 +0300

    fixed

--- js/util.js
+++ js/util.js
@@ -1,15 +1,11 @@
-const checkStringLength = (string, length) => string.length <= length;
-
 const getRandomPositiveInteger = (a, b) => {
-    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-const getRandomArrayElement = (elements) => {
-    return elements[getRandomPositiveInteger(0, elements.length -1)];
-};
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
